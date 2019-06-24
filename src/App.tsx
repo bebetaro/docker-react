0dfaf297ee08f9802e6fc403f1755e3187a143e9
takumi <takumi.kurokawa.dev@gmail.com>
2019-06-25 02:50:25 +0900

    changed app text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Changed
+         I was changed on the feature branch
         </p>
         <a
           className="App-link"
